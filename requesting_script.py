08e7e41074947a0e6c6e469f7f2a0847895a7ce1
Utkarsh Mishra <ug19981@gmail.com>
2020-02-19 17:15:41 +0530

    Delete requesting_script.py

--- requesting_script.py
+++
@@ -1,6 +0,0 @@
-import requests
-dictToSend = {'description':'Salary'}
-res = requests.get('http://localhost:5000/')
-print('response from server:',res.text)
-# dictFromServer = res.json()
-# , json=dictToSend
